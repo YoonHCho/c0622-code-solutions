f15b5a755dbd77fa41b70e74b48d0de24d8030ed
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-12 20:31:04 +0000

    still working on it

---
+++ javascript-modal/main.js
@@ -0,0 +1,9 @@
+/* var modalBtn = document.querySelector('.modal-btn');
+var noBtn = document.querySelector('.no-btn');
+var popUp = document.querySelector('pop-up');
+
+modalBtn.addEventListener('click', showModal);
+
+function showModal(event) {
+
+} */
