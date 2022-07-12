263926c02dee698e1cf186cc288c5a49942fc706
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-12 06:12:25 +0000

    completed array-manipulation-2 - YoonHwan Cho

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,10 +1,43 @@
 /* exported chunk */
 
-//     declare function named chunk with 2 parameter (array, size)
-//     initialize variable start and end, start = 0, end = size,
+/*
+    declare function named chunk with 2 parameter (array, size)
+    initialize variable endIndex = size
+    initialize variable newArray with empthy array literal
+    loop through, final expression increment by size
+    new variable temp will be assigned with slice method, start with i and end with endIndex
+    push temp to newArray
+    increament endIndex by the size
+    return newArray
+*/
 
-// function chunk(array, size) {
-//   var endIndex = size;
-//   for (var i = 0; i < array.length / size)
+// below method using splice.
+/* function chunk(array, size) {
+  var endIndex = size;
+  var newArray = [];
+  for (var i = 0; i < array.length; i += size) {
+    var temp = array.slice(i, endIndex);
+    newArray.push(temp);
+    endIndex += size;
+  }
+  return newArray;
+} */
 
-// }
+// below, not using slice method
+function chunk(array, size) {
+  var counter = size;
+  var newArray = [];
+  for (var i = 0; i < array.length; i += size) {
+    var temp = [];
+    for (var j = i; j < counter; j++) {
+      temp.push(array[j]);
+    }
+    if (array.length < counter + size) {
+      counter = array.length;
+    } else {
+      counter += size;
+    }
+    newArray.push(temp);
+  }
+  return newArray;
+}
