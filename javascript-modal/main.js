19559b099fbfc07dc05a85301c31717413238a7a
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-13 01:45:39 +0000

    still working on it

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,9 +1,20 @@
-/* var modalBtn = document.querySelector('.modal-btn');
+var modalBtn = document.querySelector('.modal');
 var noBtn = document.querySelector('.no-btn');
-var popUp = document.querySelector('pop-up');
+var popUp = document.querySelector('.pop-up');
 
-modalBtn.addEventListener('click', showModal);
+/* console.log(modalBtn);
+console.log(noBtn);
+console.log(popUp); */
 
-function showModal(event) {
+modalBtn.addEventListener('click', showPopUp);
+noBtn.addEventListener('click', hidePopUp);
 
-} */
+function showPopUp(event) {
+/*   console.log('test');
+  background.className = 'background-on'; */
+}
+
+function hidePopUp() {
+  popUp.className = 'pop-up-hide fixed';
+  /* background.className = ''; */
+}
