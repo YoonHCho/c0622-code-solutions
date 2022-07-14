60dda261a28f5fdf34e0215b799cce44df082c01
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-14 00:23:04 +0000

    replace the variable from outside function to inside the function code block

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,10 +1,10 @@
 var $contact = document.querySelector('#contact-form');
 
 $contact.addEventListener('submit', submitForm);
-var object = {};
 
 function submitForm(event) {
   event.preventDefault();
+  var object = {};
   object.name = $contact.elements.name.value;
   object.email = $contact.elements.email.value;
   object.meesage = $contact.elements.message.value;
