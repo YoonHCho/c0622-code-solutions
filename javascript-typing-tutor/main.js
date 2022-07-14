30d396bab04ab89a4494037f09d265ff1ca5e357
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-14 01:37:54 +0000

    still working

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,9 +1,7 @@
 var char = document.querySelectorAll('span');
 var index = 0;
-var typos = 0;
-document.addEventListener('keydown', checkKey);
-/* console.log(char);
-console.dir(char); */
+window.addEventListener('keydown', checkKey);
+
 function checkKey(event) {
   if (char[index].textContent === event.key) {
     char[index].className = 'correct';
@@ -11,11 +9,5 @@ function checkKey(event) {
     char[index].className = 'current';
   } else {
     char[index].className = 'incorrect';
-    typos++;
   }
-}
-
-if (char[char.length - 1].className === 'correct') {
-  var accuracy = typos / 30;
-  window.alert('You missed ' + accuracy + ' % of typing.');
 }
