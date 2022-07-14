2f39c46365396042b0551d28a364a1838126eec1
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-14 23:35:52 +0000

    completed javascript-view-swapping

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,26 @@
+var $tabContainer = document.querySelector('.tab-container');
+var $tab = document.querySelectorAll('.tab');
+var $view = document.querySelectorAll('.view');
+
+$tabContainer.addEventListener('click', toggleTab);
+
+function toggleTab(event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < $tab.length; i++) {
+      if ($tab[i] === event.target) {
+        $tab[i].className = 'tab active';
+      } else {
+        $tab[i].className = 'tab';
+      }
+    }
+
+    var $dataValue = event.target.dataset.view;
+    for (var j = 0; j < $view.length; j++) {
+      if ($view[j].dataset.view === $dataValue) {
+        $view[j].className = 'view';
+      } else {
+        $view[j].className = 'view hidden';
+      }
+    }
+  }
+}
