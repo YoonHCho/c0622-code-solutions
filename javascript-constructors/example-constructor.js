3644692d02d34d86eae0b1bb4c2ab343a1b70da4
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-26 19:30:34 +0000

    added notes

--- javascript-constructors/example-constructor.js
+++ javascript-constructors/example-constructor.js
@@ -2,6 +2,7 @@ function ExampleConstructor() {
 
 }
 
+// typeof aFunction / typeof aFunction.__proto__ / typeof ExampleConstructor.prototype / ExampleConstructor.prototype / aFunction instanceof ExampleConstructor
 console.log('value of ExampleConstructor.prototype: ', ExampleConstructor.prototype);
 console.log('typeof ExampleConstructor.prototype: ', typeof ExampleConstructor.prototype);
 
