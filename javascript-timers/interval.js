b12e298aefa509690805de80387ec4032e435229
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-27 17:55:44 +0000

    updated adjustments with DOMContentLoaded

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,11 +1,21 @@
+window.addEventListener('DOMContentLoaded', startCount);
+
 var $countDown = document.querySelector('.countdown-display');
 var counter = parseInt($countDown.textContent);
-var intervalID = setInterval(countDown, 1000);
+var intervalId;
+
+function startCount() {
+  if (!intervalId) {
+    intervalId = setInterval(countDown, 1000);
+  }
+}
 
 function countDown() {
   if (counter === 1) {
     $countDown.textContent = '~Earth Beeeelooowww Us~';
-    return clearInterval(intervalID);
+    clearInterval(intervalId);
+    intervalId = null;
+    return;
   }
   counter--;
   $countDown.textContent = counter;
