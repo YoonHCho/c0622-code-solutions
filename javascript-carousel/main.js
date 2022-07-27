5c179fce98c0c0148adf3a2f5d0800990b6c1550
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-27 23:29:47 +0000

    Completed JS carousel - YoonHwan Cho

---
+++ javascript-carousel/main.js
@@ -0,0 +1,54 @@
+var imageURL = [
+  'images/001.png',
+  'images/004.png',
+  'images/007.png',
+  'images/025.png',
+  'images/039.png'
+];
+
+var $thruImages = document.querySelector('.thru-images');
+var $circle = document.querySelectorAll('.circle');
+var currentIndex = 0;
+
+setInterval(thruImages, 3000);
+var $rightArrow = document.querySelector('.right-arrow');
+$rightArrow.addEventListener('click', thruImages);
+
+function thruImages() {
+  currentIndex++;
+  if (currentIndex < 5) {
+    $thruImages.setAttribute('src', imageURL[currentIndex]);
+    $circle[currentIndex - 1].classList.replace('fa-solid', 'fa-regular');
+    $circle[currentIndex].classList.replace('fa-regular', 'fa-solid');
+  } else {
+    $circle[currentIndex - 1].classList.replace('fa-solid', 'fa-regular');
+    currentIndex = 0;
+    $circle[currentIndex].classList.replace('fa-regular', 'fa-solid');
+    $thruImages.setAttribute('src', imageURL[currentIndex]);
+  }
+}
+
+var $leftArraow = document.querySelector('.left-arrow');
+$leftArraow.addEventListener('click', oneBefore);
+function oneBefore(event) {
+  currentIndex--;
+  if (currentIndex < 0) {
+    $circle[currentIndex + 1].classList.replace('fa-solid', 'fa-regular');
+    currentIndex = imageURL.length - 1;
+    $thruImages.setAttribute('src', imageURL[currentIndex]);
+    $circle[currentIndex].classList.replace('fa-regular', 'fa-solid');
+  } else {
+    $circle[currentIndex + 1].classList.replace('fa-solid', 'fa-regular');
+    $thruImages.setAttribute('src', imageURL[currentIndex]);
+    $circle[currentIndex].classList.replace('fa-regular', 'fa-solid');
+  }
+}
+
+var $circleList = document.querySelector('.circle-list');
+$circleList.addEventListener('click', whichCircImg);
+function whichCircImg(event) {
+  $circle[currentIndex].classList.replace('fa-solid', 'fa-regular');
+  currentIndex = event.target.getAttribute('data-id');
+  $circle[currentIndex].classList.replace('fa-regular', 'fa-solid');
+  $thruImages.setAttribute('src', imageURL[currentIndex]);
+}
