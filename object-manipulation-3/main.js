5d4925a8d33a99ca0fb72c94cbec10f7d50b0dea
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-08-10 00:09:59 +0000

    completed object-manipulation-3

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,14 +1,5 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-var converter = [
-  {
-    ace: 11,
-    jack: 10,
-    queen: 10,
-    king: 10
-  }
-];
-
 var cards = [
   {
     suit: 'Spades',
@@ -87,29 +78,40 @@ function dealCards() {
     console.log(players[playerNum].name + ' has ' + cardsInString);
     playerNum++;
   }
+  console.log('Calculating the scores, please wait');
+  setTimeout(calcScore, 5000);
 }
 
-dealCards();
-console.log('cards after deal: ', cards);
+console.log('Shuffling the deck and dealing cards, please wait');
+setTimeout(dealCards, 3500);
 
-// can call this function as calcScore
-for (var i = 0; i < players.length; i++) {
-  var score = 0;
-  for (var k = 0; k < players[i].hand.length; k++) {
-    if (Number.isInteger(players[i].hand[k][1])) {
-      score += players[i].hand[k][1];
-    } else if (players[i].hand[k][1] === 'ace') {
-      score += 11;
-    } else {
-      score += 10;
+function calcScore() {
+  for (var i = 0; i < players.length; i++) {
+    var score = 0;
+    for (var k = 0; k < players[i].hand.length; k++) {
+      if (Number.isInteger(players[i].hand[k][1])) {
+        score += players[i].hand[k][1];
+      } else if (players[i].hand[k][1] === 'ace') {
+        score += 11;
+      } else {
+        score += 10;
+      }
+    }
+    players[i].score = score;
+    console.log(players[i].name + ' has scored ' + players[i].score);
+  }
+  console.log('declaring the winner, please wait');
+  setTimeout(calcWinner, 3500);
+}
+
+function calcWinner() {
+  var highest = 0;
+  var winner = '';
+  for (var i = 0; i < players.length; i++) {
+    if (players[i].score > highest) {
+      highest = players[i].score;
+      winner = players[i].name + ' has won with highest score of ' + highest;
     }
   }
-  players[i].score = score;
-  console.log(players[i].name + ' has scored ' + players[i].score);
+  console.log(winner);
 }
-
-// need a function to declare a winner
-// function whoWins() {
-//   for ()
-// }
-console.log(converter);
