981b95281b8754215f7001eaf7fd72e9108e3549
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-09-01 01:24:10 +0000

    STILL WORKING ON IT

---
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class StopWatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: false,
+      count: 0,
+      timeId: undefined
+    };
+    this.startWatch = this.startWatch.bind(this);
+    this.pauseWatch = this.pauseWatch.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+  tick() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  startWatch() {
+    if (!this.state.start) {
+      this.setState({ start: true });
+      this.timeId = setInterval(this.tick, 1000);
+    }
+  }
+
+  pauseWatch() {
+    if (!this.state.start) {
+      this.setState({ count: 0 });
+    }
+    this.setState({ start: false });
+    clearInterval(this.timeId);
+  }
+
+  render() {
+    return (
+
+      <div className='row'>
+        <div className='col-100'>
+          <button onClick={ this.pauseWatch }>{ this.state.count }</button>
+        </div>
+        <div className='col-100'>
+          <i onClick={ this.state.start ? this.pauseWatch : this.startWatch} className={ !this.state.start ? 'fa-solid fa-play' : 'fa-solid fa-pause'}></i>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default StopWatch;
