6418c6f95707b5782191dd0ce55dcaa62a03b70c
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-09-09 22:18:39 +0000

    completed react-context

--- react-context/client/components/auth-form.jsx
+++ react-context/client/components/auth-form.jsx
@@ -32,9 +32,6 @@ export default class AuthForm extends React.Component {
         if (action === 'sign-up') {
           window.location.hash = 'sign-in';
         } else if (result.user && result.token) {
-          // console.log(result.user); // userId 2 username username
-          // console.log(result.token); // the token
-          // console.log(this.props.onSignIn); // ahndleSignin from app.jsx
           this.props.onSignIn(result);
         }
       });
