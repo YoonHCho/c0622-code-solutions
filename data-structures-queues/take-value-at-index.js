609ce330ad63f138187246f03c62ba15c1c89c94
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-09-27 22:45:07 +0000

    completed data structures queue

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,33 +1,15 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
-  // let counter = 1;
-  // if (queue.peek() === undefined) return;
-  // let first = queue.dequeue();
-  // while (queue.peek() !== undefined || counter <= index) {
-  //   if (counter === index) {
-  //     queue.dequeue();
-  //     return queue;
-  //   }
-  //   queue.enqueue(first);
-  //   first = queue.dequeue();
-  //   counter++;
-  // }
-  // return queue;
-
-  // while (counter <= index) {
-  //   if (counter === index) {
-  //     queue.dequeue();
-  //     return queue;
-  //   } else if (index === 0) return 0;
-  //   const toBack = queue.dequeue();
-  //   if (queue.peek() === undefined) {
-  //     counter++;
-  //     return '<empty>';
-  //   }
-  //   counter++;
-  //   console.log(toBack);
-  //   queue.enqueue(toBack);
-  // }
-  // return queue;
+  let counter = 0;
+  if (queue.peek() === undefined) return;
+  while (counter <= index) {
+    if (counter === index) {
+      const atIndex = queue.dequeue();
+      return atIndex;
+    }
+    counter++;
+    const first = queue.dequeue();
+    queue.enqueue(first);
+  }
 }
